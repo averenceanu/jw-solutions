396225a7e04343b993f0e5abd7d1e75ccca32a7e
averenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-02-28 10:19:20 -0500

    create style sheet for navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import NavBar from "./components/navbar/NavBar.js"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Your App will be here! </h1>
-      </header>
+      <NavBar/> 
     </div>
   );
 }
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,18 @@
+import './navbar.css'
+
+export default function NavBar () {
+  return (
+    <div className="navbar"> 
+      <div className="navbar--logo">
+        Logo
+      </div>
+      <div className="navbar--buttons">
+        About Us
+        Services 
+        Careers
+        Contact
+        Email Us 
+      </div>
+    </div>
+  )
+}
