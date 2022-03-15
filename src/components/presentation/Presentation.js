ae0ff2ffae49d069e2073eb6c17cb4d6ca8fe178
averenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-03-15 19:45:21 -0400

    add about background image and add buttons presentation

--- src/components/presentation/Presentation.js
+++ src/components/presentation/Presentation.js
@@ -1,6 +1,5 @@
-import {useState, useRef, useEffect} from "react"; 
 import "./presentation.css"
-import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button'
 
 export default function Presentation () { 
   return (
@@ -10,8 +9,15 @@ export default function Presentation () {
           <h1 className='presentation--it' >IT </h1>
           <h1 className='presentation--coorporate'>Solutions</h1>
         </div>
+        <div className="presentation--logo">
+          <p>Consulting. Developing. Innovating.</p>
+        </div>
         <div className="presentation--description">
-          <p>Consulting. Developing. Building.</p>
+          <p>Our goal is to provide your business with all the tools to best use technology and software for maximal benefit. </p>
+        </div> 
+        <div className="presentation--button">
+          <Button color='secondary'> About Us </Button>
+          <Button color='info' variant='contained'> Contact Us </Button>
         </div>
     </div>
   )
