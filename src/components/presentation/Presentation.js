0a664b991ca979d89414f7f0f3662d200e93f1ce
averenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-03-15 17:34:16 -0400

    add presentation title animation

--- src/components/presentation/Presentation.js
+++ src/components/presentation/Presentation.js
@@ -1,9 +1,15 @@
+import {useState, useRef, useEffect} from "react"; 
 import "./presentation.css"
+import Typography from '@mui/material/Typography';
 
 export default function Presentation () { 
   return (
-    <div className = "presentation" style ={{backgroundImage: `url(/images/baner.png)`}}>
-      <p> Hello</p>
+    <div className = "presentation" style ={{backgroundImage: `url(/images/baner.png)`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', width: '100vw', height: '100vh'}}>
+        <div className="presentation--title"> 
+          <h1 className='presentation--coorporate'>Corporate</h1>
+          <h1 className='presentation--it' >IT </h1>
+          <h1 className='presentation--coorporate'>Solutions</h1>
+        </div>
     </div>
   )
 }
